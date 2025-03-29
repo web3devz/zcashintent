cfddc7f7bf0d11c94bb495d5bbfef3be41714271
web3 Developerz <101476931+web3devz@users.noreply.github.com>
2025-03-29 12:12:13 +0000

    near added

--- src/utils/renderAppLink.tsx
+++ src/utils/renderAppLink.tsx
@@ -4,7 +4,7 @@ import type { ReactNode } from "react"
 import { useSignInWindowOpenState } from "@src/stores/useSignInWindowOpenState"
 
 export function renderAppLink(
-  routeName: "withdraw" | "deposit" | "gift" | "sign-in" | "swap" | "otc",
+  routeName: "withdraw" | "deposit" | "gift" | "sign-in" | "swap" ,
   children: ReactNode,
   props: { className?: string }
 ) {
@@ -43,12 +43,6 @@ export function renderAppLink(
     case "swap":
       return (
         <Link href="/" {...props}>
-          {children}
-        </Link>
-      )
-    case "otc":
-      return (
-        <Link href="/otc-desk/create-order" {...props}>
           {children}
         </Link>
       )
